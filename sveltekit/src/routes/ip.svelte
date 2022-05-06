5aa718f7ef41ded8a2265c2dcacbe1739fae78ea
Alex Ma <66192750+RyukCEO@users.noreply.github.com>
2022-05-05 22:57:11 -0700

    features

---
+++ sveltekit/src/routes/ip.svelte
@@ -0,0 +1,39 @@
+<script>
+    function get(event) {
+	return {
+		body: {
+			ip: event.clientAddress
+		}
+	};
+}
+
+
+	import '../app.css';
+	import { page } from '$app/stores';
+	/** @type {string} */
+	export let city;
+	/** @type {string} */
+	export let ip;
+</script>
+
+
+
+
+
+
+<main>
+	<div class="info">
+		<div class="block">
+			<div class="contents">
+				<span>Your city</span>
+				<strong>{city}</strong>
+			</div>
+		</div>
+		<div class="block">
+			<div class="contents">
+				<span>Your IP address</span>
+				<strong>{ip}</strong>
+			</div>
+		</div>
+	</div>
+</main>
