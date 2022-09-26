1e2fbc3deafeddb6b423bdfd4481b20761dd2aaa
Alex Ma <66192750+RyukCEO@users.noreply.github.com>
2022-09-25 22:04:48 -0700

    image feature

--- sveltekit/src/app.d.ts
+++ sveltekit/src/app.d.ts
@@ -1,3 +1,16 @@
+/// <reference types="@sveltejs/kit" />
+
+// See https://kit.svelte.dev/docs/types#app
+// for information about these interfaces
+declare namespace App {
+	interface Locals {
+		user?: import('@api').UserResponseDto;
+	}
+
+	// interface Platform {}
+}
+
+
 declare namespace App {
 	interface Locals {
 		iOS: boolean;
